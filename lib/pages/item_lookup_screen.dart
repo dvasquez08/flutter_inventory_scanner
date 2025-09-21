1a937f0421f3ac7640570d7e01e9cbc14c473548
David Vasquez <dvasquez.422@gmail.com>
2025-09-21 06:49:20 -0600

    Added the lookup and add item screen, no config yet

---
+++ lib/pages/item_lookup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemLookupScreen extends StatefulWidget {
+  const ItemLookupScreen({super.key});
+
+  @override
+  State<ItemLookupScreen> createState() => _ItemLookupScreenState();
+}
+
+class _ItemLookupScreenState extends State<ItemLookupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
