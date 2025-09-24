20224ceb01e5cc9622615e2e641f4b0eb1be5b80
David Vasquez <dvasquez.422@gmail.com>
2025-09-24 16:07:51 -0600

    Adjusted the SansText widget to adjust colour and weight

--- lib/components.dart
+++ lib/components.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SansText extends StatelessWidget {
-  final text;
-  final size;
-  const SansText(this.text, this.size, {super.key});
+  final String text;
+  final double size;
+  // Make color and weight optional parameters for more flexibility
+  final Color color;
+  final FontWeight weight;
+
+  const SansText(
+    this.text,
+    this.size, {
+    super.key,
+    this.color = Colors.white, // Default to white if not provided
+    this.weight = FontWeight.w300, // Default to light weight
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      textAlign: TextAlign.center,
+      textAlign:
+          TextAlign.center, // Consider making this an optional parameter too!
       style: GoogleFonts.openSans(
         fontSize: size,
-        fontWeight: FontWeight.w300,
-        color: Colors.white,
+        fontWeight: weight,
+
+        color: color,
       ),
     );
   }
